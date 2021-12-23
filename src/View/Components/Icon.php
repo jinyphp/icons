d266acfd89e87be1027cc3282122837d32646960
hojinlee <infohojin@gmail.com>
2021-12-23 17:39:19 +0900

    component sub dir

--- src/View/Components/Icon.php
+++ src/View/Components/Icon.php
@@ -25,8 +25,10 @@ class Icon extends Component
         ## svg 파일 경로
         $path = __DIR__.'/../../../resources/views';
 
+        $name = str_replace(".",DIRECTORY_SEPARATOR,$this->name);
+
         ## svg 파일 읽기
-        $filename = $path.DIRECTORY_SEPARATOR.$this->type.DIRECTORY_SEPARATOR.$this->name.".svg";
+        $filename = $path.DIRECTORY_SEPARATOR.$this->type.DIRECTORY_SEPARATOR.$name.".svg";
         if(file_exists($filename)) {
             $svg = file_get_contents($filename);
         } else {
